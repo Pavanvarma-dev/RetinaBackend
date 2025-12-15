a1e7c599c7b9a13b7abd6ad3232743f6b017d86a
Pavanvarma-dev <pavanvarma758@gmail.com>
2025-12-15 20:28:09 +0530

    changes done for aws RDS

--- util/db.js
+++ util/db.js
@@ -1,10 +1,27 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('Retina','postgres','System123',{
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+    host:process.env.DB_HOST,
     dialect:'postgres',
-    host:'localhost',
     logging:console.log,
-    port:5432
+    port:5432,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch(err => {
+    console.error('Unable to connect to the database:', err);
 })
 
 module.exports = sequelize;
